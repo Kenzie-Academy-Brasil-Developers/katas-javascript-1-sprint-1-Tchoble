59d063a2c8eb82721247e841edd1fe95ba6a0b8c
Tchoble <leandersonlopes1@yahoo.com.br>
2021-09-14 23:15:20 -0300

    Revisao dos códigos

--- katas1.js
+++ katas1.js
@@ -1,106 +1,156 @@
+// de 1 a 20
 
-//de 1 a 20
+function umAte20() {
+    
+    let meuRetorno = [];
 
-umAte20(20)
-function umAte20(limite){
-for (let i = 1; i <= limite; i++) {
-console.log('Numero ',i);
-}
-return umAte20;
-}
-
-//Numeros pares de 0 a 20
-parAte20(20)
-function parAte20(limite){
-for (let i = 0; i <= limite; i++) {
-if (i % 2 === 0)
-console.log(i, 'Numero Par');
-}
-return parAte20;
+    for (let i = 1; i <= 20; i++) {
+ 
+    
+    return meuRetorno;
+    
 }
 
+console.log(umAte20());
+} 
 
+//pares de 1 a 20
+function parAte20() {
+    
+    let par20 = [];
 
+    for (let i = 1; i <= 20; i++) {
+        if (i % 2 ===0)
+    
+    return par20;
+    
+}
+
+console.log(parAte20());
+}
 
 //impares de 1 a 20
 
-imparAte20(20)
-function imparAte20(limite){
-for (let i = 1; i <= limite; i++) {
-if (i % 3 === 0)
-console.log(i, 'Numero impar');
+function imparAte20() {
+    
+    let impar20 = [];
+
+    for (let i = 1; i <= 20; i++) {
+        if (i % 3 ===0)
+    
+    return impar20;
+    
 }
-return imparAte20;
-}
 
-
-
-//de 5 a 100
-
-mtpAte100(100)
-function mtpAte100(limite){
-for (let i = 1; i <= limite; i++) {
-if (i % 5 === 0)
-console.log(i, 'Multiplo de 5');
-}
-return mtpAte100;
+console.log(imparAte20());
 }
 
 
-//call function multiplesOfFive
+//multiplo de 5 ate 100
+function multAte100() {
+    
+    let m100 = [];
 
-//Não consegui achar um resultado
-
-
-// conta de 20 a 1
-
-vinteAteUm(1)
-function vinteAteUm(limite){
-for (let i = 20; i >= limite; i--) {
-console.log('Numero ',i);
-}
-return vinteAteUm;
+    for (let i = 1; i <= 100; i++) {
+        if (i % 5 ===0)
+    
+    return m100;
+    
 }
 
-
-// numeros pares de 20 a 1
-
-
-
-parVinteAteUm(1)
-function parVinteAteUm(limite){
-for (let i = 20; i >= limite; i--) {
-if (i % 2 === 0)
-console.log(i, 'Numero Par');
-}
-return parVinteAteUm;
+console.log(multAte100());
 }
 
+//quadrado perfeito
 
+function quadAte100() {
+    
+    let q100 = [];
 
-
-// impares de 20 a 1
-
-imparVinteAteUm(1)
-function imparVinteAteUm(limite){
-for (let i = 20; i >= limite; i--) {
-if (i % 3 === 0)
-console.log(i, 'Numero impar');
-}
-return imparVinteAteUm;
-}
-
-
-// multiplos de 100 a 5
-mtpInverso(1)
-function mtpInverso(limite){
-for (let i = 100; i >= limite; i--) {
-if (i % 5 === 0)
-console.log(i, 'Numero múltiplo de 5');
-}
-return mtpInverso;
+    for (let i = 1; i <= 100; i++) {
+        if (i ** 2 == 0)
+    
+    return q100;
+    
 }
 
-//numero multiplo inverso
+console.log(quadAte100());
+}
 
-//Não consegui achar um resultado
+//vinte a um
+
+function vinteA1() {
+    
+    let v1 = [];
+
+    for (let i = 20; i >= 1; i--) {
+    
+    return v1;
+    
+}
+
+console.log(vinteA1());
+
+}
+
+//par vinte a um
+
+function pvinteA1() {
+    
+    let pv1 = [];
+
+    for (let i = 20; i >= 1; i--) {
+        if (i % 2 ===0)
+    return pv1;
+    
+}
+
+console.log(pvinteA1());    
+}
+
+//impares de 20 a 1
+
+function impar20A1() {
+    
+    let i20A1 = [];
+
+    for (let i = 20; i >= 1; i--) {
+        if (i % 3 ===0)
+    
+    return i20A1;
+    
+}
+
+console.log(impar20A1());
+}
+
+//multiplo de 100 a 5
+function m100A5() {
+    
+    let inv100A5 = [];
+
+    for (let i = 100; i >= 5; i--) {
+        if (i % 5 ===0)
+    
+    return inv100A5;
+    
+}
+
+console.log(m100A5());
+}
+
+//quadrado perfeito inverso
+
+function quad100Inv() {
+    
+    let q100Inv = [];
+
+    for (let i = 1; i <= 100; i++) {
+        if (i ** 2 == 0)
+    
+    return q100Inv;
+    
+}
+
+console.log(quad100Inv());
+}
